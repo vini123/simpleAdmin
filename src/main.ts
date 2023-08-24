c2c64fa4a717dbee757e7545bfab4500c7d96011
zhoulin <zhoulin@xiangrong.pro>
2023-08-24 09:41:52 +0800

    安装 default-passive-events 来解决 touch start 中阻止默认行为

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/assets/svg-icon/svg-icon.ts'
+import "default-passive-events";
 
 const app = createApp(App)
 
